8127e27a08fcd000087ed6c79f2182540fffd767
Brandon Mellus <mellusbrandon@gmail.com>
2021-10-22 14:04:42 -0700

    Define typedefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,6 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    User {
+        username: String
+        email: String
+        savedBooks: [Book]
+    }
+
     type Query {
         test: String
     }
